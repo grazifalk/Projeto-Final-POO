4dc8e88da177d80c1302a37e2bd4b435f1a73bde
Rafael Gomez <rafaelgomez@MacBook-Pro-de-Rafael.local>
2022-09-16 13:51:55 -0300

    Diretor inserido no LeituraEscrita

--- src/br/com/projetofinal/IO/LeituraEscrita.java
+++ src/br/com/projetofinal/IO/LeituraEscrita.java
@@ -8,35 +8,45 @@ import br.com.poo.projetofinal.contas.Conta;
 import br.com.poo.projetofinal.contas.ContaCorrente;
 import br.com.poo.projetofinal.enums.ETipoConta;
 import br.com.poo.projetofinal.enums.ETipoFuncionario;
+import br.com.poo.projetofinal.funcionario.Diretor;
 import br.com.poo.projetofinal.funcionario.Funcionario;
 import br.com.poo.projetofinal.funcionario.Gerente;
 
 public class LeituraEscrita {
-	
+
 	static final String PATH_BASICO = "./temp/";
 	static final String EXTENSAO = ".txt";
-	
+
 	public static void leitor(String path) throws IOException {
 		BufferedReader buffRead = new BufferedReader(new FileReader(PATH_BASICO + path + EXTENSAO));
-		
+
 		String linha = "";
-		
+
 		while (true) {
 			linha = buffRead.readLine();
 			if (linha != null) {
 				String[] dados = linha.split(";");
 				if (dados[0].equalsIgnoreCase(ETipoFuncionario.GERENTE.getTipoFuncionario())) {
-					Gerente gerente = new Gerente(dados[0], dados[1], dados[2], dados[3],
-							Integer.parseInt(dados[4]), Integer.parseInt(dados[5]), Double.parseDouble(dados[6]),
-							Integer.parseInt(dados[7]));
+					Gerente gerente = new Gerente(dados[0], dados[1], dados[2], dados[3], Integer.parseInt(dados[4]),
+							Integer.parseInt(dados[5]), Double.parseDouble(dados[6]), Integer.parseInt(dados[7]));
 					Funcionario.mapaFuncionarios.put(dados[2], gerente);
 					Funcionario.OrdenaFuncionarios.put(dados[0], gerente);
-
+					
+				}	else if (dados[0].equalsIgnoreCase(ETipoFuncionario.DIRETOR.getTipoFuncionario())) {
+					Diretor diretor = new Diretor(dados[0], dados[1], dados[2], dados[3], Integer.parseInt(dados[4]),
+							Integer.parseInt(dados[5]), Double.parseDouble(dados[6]));
+					Funcionario.mapaFuncionarios.put(dados[2], diretor);
+					Funcionario.OrdenaFuncionarios.put(dados[1], diretor);
 				}
+				
 			} else {
-			break;
+				break;
+			}
 		}
+		buffRead.close();
 	}
-	buffRead.close();
-}
-}
+}	
+					
+				
+
+	
